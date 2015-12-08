83c71da3adf11e7dc1903440fc0693570576c56f
Martin Takac <martin.taki@gmail.com>
2015-12-08 13:21:37 -0500

    creating build scripts

---
+++ src/cocoa.cpp
@@ -0,0 +1,37 @@
+/*
+ ============================================================================
+ Name        : CoCoA.c
+ Author      : Dr. Martin Takac, Chenxin Ma
+ Version     :
+ Copyright   : 
+ Description : CoCoA algorithm
+ Based on following papers:
+    1.  Jaggi, Martin, Virginia Smith, Martin Takác, Jonathan Terhorst,
+        Sanjay Krishnan, Thomas Hofmann, and Michael I. Jordan.
+        "Communication-efficient distributed dual coordinate ascent."
+        In Advances in Neural Information Processing Systems, pp. 3068-3076. 2014.
+    2.  Chenxin Ma, Virginia Smith, Martin Jaggi, Michael I Jordan, Peter Richtárik, Martin Takáč
+        "Adding vs. averaging in distributed primal-dual optimization."
+        In 32th International Conference on Machine Learning, 2015.
+    3.  TODOCOCOA3PAPER
+
+ ============================================================================
+ */
+#include <math.h> 
+#include "mpi.h"
+#include <iostream>
+using namespace std;
+ 
+int main(int argc, char *argv[]) {
+
+	MPI::Init(argc, argv);
+
+	cout << "asfddddddddddddasfa  dddddd"<<endl;
+
+
+
+
+	MPI::Finalize();
+	return 0;
+}
+
